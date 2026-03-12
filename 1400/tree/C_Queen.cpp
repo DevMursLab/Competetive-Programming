758000f759610bdab62d81301848a9727657be97
programmer-mursalin <programmer.mursalin016@gmail.com>
2026-03-12 15:51:10 +0600

    tree

---
+++ 1400/tree/C_Queen.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+#define int long long
+#define gcd __gcd
+
+#define ALL(x) (x).begin(), (x).end()
+#define py cout << "YES\n";
+#define pm cout << "-1\n";
+#define pz cout << "0\n";
+#define pn cout << "NO\n";
+#define cheakmate return;
+const int N = 1e5 + 5;
+#define Mod 1000000009 + 7
+
+void solve()
+{
+
+    // 2d input
+    // vector<vector< int>> d(n, vector< int>(m));
+    //   int n;
+    // cin >> n;
+    // vector< int> a(n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> a[i];
+    //}
+    int n;
+    cin >> n;
+    vector<bool> good(n);
+    vector<vector<int>> adj(n);
+    for (int i = 0; i < n; i++)
+    {
+        int parent, type;
+        cin >> parent >> type;
+        if (type == 0)
+            good[i] = true;
+        if (parent != -1)
+            adj[parent - 1].push_back(i);
+    }
+    vector<int> ans;
+    for (int i = 0; i < n; i++)
+    {
+        if (good[i])
+            continue;
+        int ok = 1;
+        for (auto it : adj[i])
+        {
+            if (good[it])
+                ok = 0;
+        }
+        if (ok)
+            ans.push_back(i + 1);
+    }
+    if (ans.size())
+    {
+        for (auto it : ans)
+            cout << it << " ";
+        cheakmate
+    }
+    pm
+}
+// sort(ALL(a),greater<int>());
+// int maxi=*max_element(a.begin(),a.end());
+//  int maxi = distance(a.begin(), max_element(a.begin(), a.end()));   // return max index
+
+signed main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
+
+/*===============================================
+  :::::::::::::Author :Md.Mursalin:::::::::::::
+  ===============================================*/
